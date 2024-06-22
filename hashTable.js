06dc3fdae2d6b1603ad82e3641032690a848719a
Alex <aamorgun2003@gmail.com>
2024-06-22 10:59:35 -0600

    Add set and check for duplicate keys method

--- hashTable.js
+++ hashTable.js
@@ -1,15 +1,49 @@
+class HashInput {
+  constructor(key, value) {
+    this.key = key;
+    this.value = value;
+  }
+}
+
 class HashTable {
   constructor(bucket) {
     this.bucket = new Array(bucket);
+    this.loadFactor = 0.8;
   }
   hash(key) {
     let hashCode = 0;
 
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
-      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.bucket.length;
+      hashCode =
+        (primeNumber * hashCode + key.charCodeAt(i)) % this.bucket.length;
     }
 
     return hashCode;
   }
+  checkDuplicateKeys(key, value) {
+    for (let entry of this.bucket) {
+      if (entry && entry.key === key) {
+        entry.value = value;
+        return true;
+      }
+    }
+    return false;
+  }
+  set(key, value) {
+    if (this.checkDuplicateKeys(key, value)) {
+      return;
+    }
+    const newHashEntry = new HashInput(key, value);
+
+    const keyHashed = this.hash(key);
+
+    this.bucket[keyHashed] = newHashEntry;
+  }
 }
+
+const hashTable = new HashTable(16);
+hashTable.set("Blah", "blah-surname");
+hashTable.set("Alex", "morgun");
+hashTable.set("Malcolm", "duren");
+hashTable.set("Arnold", "schwarnager");
